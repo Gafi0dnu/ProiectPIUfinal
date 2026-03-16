b4e32f41ef55219475a94eaa8fada6c6851abca9
Gafi <Gafi@DESKTOP-Q25EJFD>
2026-03-16 16:04:48 +0200

    Add project files.

---
+++ ProiectPIUfinal/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+
+namespace Transporturi
+{
+    // lab 1 facem read me pe git si scriem succint ce face aplicatia noastra
+    // lab 2 facem clasele pe care le folosim aprox, macar 3 clase, si metodele
+    // lab 3 luam o clasa si creem functii pt a incarca date, un ajutor(nume grupa idee etc), exit etc
+
+    public class Sofer
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Categorie { get; set; }
+        public double KilometriiTotali { get; set; }
+
+        public override string ToString() => $"{Nume} {Prenume}";
+    }
+
+    public class Masina
+    {
+        public string Marca { get; set; }
+        public string Model { get; set; }
+        public double Kilometraj { get; set; }
+        public override string ToString() => $"{Marca} {Model}";
+    }
+
+    public class Traseu
+    {
+        public Sofer Sofer { get; set; }
+        public Masina Masina { get; set; }
+        public DateTime DataStart { get; set; }
+        public DateTime DataEnd { get; set; }
+        public string Cursa { get; set; }
+        public double Kilometrii { get; set; }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicatie Transporturi");
+
+            var sofer = new Sofer
+            {
+
+            };
+
+            var masina = new Masina
+            {
+
+            };
+
+            var traseu = new Traseu
+            {
+
+            };
+
+
+        }
+    }
+}
